36b23a9d982d1f67d053b13ef6b80a83fcddf3d7
D0ng3r <128439825+May0Need5@users.noreply.github.com>
2025-04-02 02:42:02 -0400

    Add files via upload

---
+++ data/pop.js
@@ -0,0 +1,321 @@
+function showModalPopUp() {
+
+    var popupWindow = window.open("/StreamList.html?core=opera&file=MortalKombat2_3DO_RC5.zip", "ModalPopUp", "width=960,height=620", "left=200", "top=50");
+
+    popupWindow.focus();
+   
+    };
+
+    function showModalPopUp2() {
+	
+	var popupWindow = window.open("/StreamList.html?core=segaCD&file=Mortal_Kombat-SegaCD.zip", "ModalPopUp", "left=200", "top=50");
+	
+	popupWindow.focus();
+	
+	};
+
+    function showModalPopUp3() {
+	
+        var popupWindow = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_(EU).gb", "ModalPopUp", "left=200", "top=50");
+        
+        popupWindow.focus();
+        
+        };
+
+        function showModalPopUp4() {
+	
+            var popupWindow2 = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_(J).gb", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+            function showModalPopUp5() {
+	
+                var popupWindow2 = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_3_(E).gb", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+                function showModalPopUp6() {
+	
+                    var popupWindow2 = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_3_(U).gb", "ModalPopUp", "left=200", "top=50");
+                    
+                    popupWindow2.focus();
+                    
+                    };
+
+                    function showModalPopUp7() {
+	
+                        var popupWindow2 = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_II_(EU).gb", "ModalPopUp", "left=200", "top=50");
+                        
+                        popupWindow2.focus();
+                        
+                        };
+
+        function showModalPopUp8() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=gb&file=Mortal_Kombat_II_Kyuukyoku_Shinken_(J).gb", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+            function showModalPopUp9() {
+
+                var popupWindow2 = window.open("/StreamList.html?core=nes&file=MortalKombatTrilogy.nes", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+    function showModalPopUp10() {
+
+        var popupWindow2 = window.open("/StreamList.html?core=snes&file=MortalKombat(U).smc", "ModalPopUp", "left=200", "top=50");
+        
+        popupWindow2.focus();
+        
+        };
+
+        function showModalPopUp11() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=snes&file=MortalKombatII(V1.0)(U).smc", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+        function showModalPopUp12() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=snes&file=MortalKombatII(V1.1)(U).smc", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+            function showModalPopUp13() {
+
+                var popupWindow2 = window.open("/StreamList.html?core=snes&file=MortalKombat3(U).smc", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+        function showModalPopUp14() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=snes&file=UltimateMortalKombat3(U).smc", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+            function showModalPopUp15() {
+
+                var popupWindow2 = window.open("/StreamList.html?core=n64&file=mktril.rom", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+                function showModalPopUp16() {
+
+                    var popupWindow2 = window.open("/StreamList.html?core=n64&file=MortalKombat4(U).z64", "ModalPopUp", "left=200", "top=50");
+                    
+                    popupWindow2.focus();
+                    
+                    };
+
+                    function showModalPopUp17() {
+
+                        var popupWindow2 = window.open("/StreamList.html?core=n64&file=MortalKombatTrilogy(U)(V1.2).z64", "ModalPopUp", "left=200", "top=50");
+                        
+                        popupWindow2.focus();
+                        
+                        };
+
+        function showModalPopUp18() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=jaguar&file=mk-jag-alpha-v0.1.0.rom", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+            function showModalPopUp19() {
+
+                var popupWindow2 = window.open("/StreamList.html?core=jaguar&file=mk-jag-alpha-v0.2.0.rom", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+function showModalPopUp20() {
+
+    var popupWindow2 = window.open("/StreamList.html?core=segaGG&file=MortalKombat(EU).gg", "ModalPopUp", "left=200", "top=50");
+    
+    popupWindow2.focus();
+    
+    };
+
+    function showModalPopUp21() {
+
+        var popupWindow2 = window.open("/StreamList.html?core=segaGG&file=MortalKombatII(W).gg", "ModalPopUp", "left=200", "top=50");
+        
+        popupWindow2.focus();
+        
+        };
+        
+
+
+
+        function showModalPopUp22() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=MortalKombat.bin", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+        
+            function showModalPopUp23() {
+        
+                var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=MortalKombatII.bin", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+                function showModalPopUp24() {
+
+                    var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=MortalKombat3.bin", "ModalPopUp", "left=200", "top=50");
+                    
+                    popupWindow2.focus();
+                    
+                    };
+                
+                    function showModalPopUp25() {
+                
+                        var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=UltimateMortalKombat3.bin", "ModalPopUp", "left=200", "top=50");
+                        
+                        popupWindow2.focus();
+                        
+                        };
+
+                        function showModalPopUp26() {
+
+                            var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=UltimateMortalKombatTrilogy.bin", "ModalPopUp", "left=200", "top=50");
+                            
+                            popupWindow2.focus();
+                            
+                            };
+                        
+                            function showModalPopUp27() {
+                        
+                                var popupWindow2 = window.open("/StreamList.html?core=segaMD&file=MortalKombatRevelations.bin", "ModalPopUp", "left=200", "top=50");
+                                
+                                popupWindow2.focus();
+                                
+                                };
+
+
+    function showModalPopUp28() {
+
+        var popupWindow2 = window.open("/StreamList.html?core=arcade&file=mk.zip", "ModalPopUp", "left=200", "top=50");
+        
+        popupWindow2.focus();
+        
+        };
+
+        function showModalPopUp29() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=arcade&file=mk2.zip", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+        
+            function showModalPopUp30() {
+        
+                var popupWindow2 = window.open("/StreamList.html?core=mame2003_plus&file=mk2.zip", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+
+
+
+    function showModalPopUp31() {
+
+        var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombat-SpecialForces(U).chd", "ModalPopUp", "left=200", "top=50");
+        
+        popupWindow2.focus();
+        
+        };
+
+        function showModalPopUp32() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatII(U).chd", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+        
+            function showModalPopUp33() {
+        
+                var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatII(J).chd", "ModalPopUp", "left=200", "top=50");
+                
+                popupWindow2.focus();
+                
+                };
+
+                function showModalPopUp34() {
+
+                    var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombat3(U).chd", "ModalPopUp", "left=200", "top=50");
+                    
+                    popupWindow2.focus();
+                    
+                    };
+            
+                    function showModalPopUp35() {
+            
+                        var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombat4(U).chd", "ModalPopUp", "left=200", "top=50");
+                        
+                        popupWindow2.focus();
+                        
+                        };
+                    
+                        function showModalPopUp36() {
+                    
+                            var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatMythologies-Sub-Zero(U).chd", "ModalPopUp", "left=200", "top=50");
+                            
+                            popupWindow2.focus();
+                            
+                            };
+
+                            function showModalPopUp37() {
+
+                                var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatTrilogy-TournamentEditionv2.0(U)(v1.1).chd", "ModalPopUp", "left=200", "top=50");
+                                
+                                popupWindow2.focus();
+                                
+                                };
+                        
+                                function showModalPopUp38() {
+                        
+                                    var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatTrilogy(U)(v1.1).chd", "ModalPopUp", "left=200", "top=50");
+                                    
+                                    popupWindow2.focus();
+                                    
+                                    };
+    
+        function showModalPopUp39() {
+
+            var popupWindow2 = window.open("/StreamList.html?core=psx&file=MortalKombatII(U).PBP", "ModalPopUp", "left=200", "top=50");
+            
+            popupWindow2.focus();
+            
+            };
+
+
+
